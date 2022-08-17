7dd037b4c9259d310b37456f86713eafe550c307
Sahil Harpal <56965873+Sahil1479@users.noreply.github.com>
2022-08-17 12:33:17 +0530

    Added function to display email id on hover
    This feature is added for message thread header part.

--- django/media/js/main.js
+++ django/media/js/main.js
@@ -53,7 +53,7 @@ function monthlistSearch() {
 
 
  /**
-   * Copy message id to clipboard
+   * Copy message if to clipboard
    */
  function copyMessageID() {
     var copyText = document.getElementById("messageId-text");
@@ -67,3 +67,49 @@ function monthlistSearch() {
     var tooltip = document.getElementById("messageidTooltip");
     tooltip.innerHTML = "Copy to clipboard";
   }
+
+
+/**
+ * Show email id on hover
+ */
+
+$(document).ready(function () {
+    //init pop-ups
+    $(".popup").attr("data-close", false);
+
+    //click on pop-up opener.
+    //pop-up is expected to be a child of opener
+    $(".more_info").click(function () {
+        var $title = $(this).find(".popup");
+        //open if not marked for closing
+        if ($title.attr("data-close") === "false") {
+            $title.show();
+        }
+        //reset popup         
+        $title.attr("data-close", false);
+    });
+
+    //mark pop-up for closing if clicked on
+    //close is initiated by document.mouseup, 
+    //marker will stop opener from re-opening it
+    $(".popup").click(function () {
+        $(this).attr("data-close",true);
+    });
+
+    //hide all pop-ups
+    $(document).mouseup(function () {
+        $(".popup").hide();
+    });
+
+    //show on rollover if mouse is used
+    $(".more_info").mouseenter(function () {
+        var $title = $(this).find(".popup");
+        $title.show();
+    });
+
+    //hide on roll-out
+    $(".more_info").mouseleave(function () {
+        var $title = $(this).find(".popup");
+        $title.hide();
+    });  
+});
